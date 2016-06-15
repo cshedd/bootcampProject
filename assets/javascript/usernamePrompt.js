5d37548fdfbb330e06ef169a4687dc5500c93174
Andrew <andrew.j.heinke@gmail.com>
2016-06-14 21:16:52 -0500

    major html updates

--- assets/javascript/usernamePrompt.js
+++ assets/javascript/usernamePrompt.js
@@ -1,27 +1,27 @@
-var dataRef = new Firebase('https://webdevprojectut.firebaseio.com/');
-
-function promptUserName() {
-  swal({
-    title: "Hello there!",
-    text: "Please enter your name:",
-    type: "input",
-    showCancelButton: false,
-    closeOnConfirm: true,
-    confirmButtonText: "Submit",
-    inputPlaceholder: "Type your name"},
-
-  function(nameInput) {
-    if (nameInput === false) return false;
-    if (nameInput === "") {
-      swal.showInputError("Please enter your name:");
-      return false;
-    }
-    var userData = {
-		    name: nameInput
-	};
-    dataRef.push(userData);
-
-  });
-}
-
-promptUserName();
+// var dataRef = new Firebase('https://webdevprojectut.firebaseio.com/');
+//
+// function promptUserName() {
+//   swal({
+//     title: "Hello there!",
+//     text: "Please enter your name:",
+//     type: "input",
+//     showCancelButton: false,
+//     closeOnConfirm: true,
+//     confirmButtonText: "Submit",
+//     inputPlaceholder: "Type your name"},
+//
+//   function(nameInput) {
+//     if (nameInput === false) return false;
+//     if (nameInput === "") {
+//       swal.showInputError("Please enter your name:");
+//       return false;
+//     }
+//     var userData = {
+// 		    name: nameInput
+// 	};
+//     dataRef.push(userData);
+//
+//   });
+// }
+//
+// promptUserName();
