1b3a4cafe1ad65e6543b1cf72debf18186b0df24
Andrew <andrew.j.heinke@gmail.com>
2016-06-16 20:21:10 -0500

    made edits in username js file

--- assets/javascript/usernamePrompt.js
+++ assets/javascript/usernamePrompt.js
@@ -1,31 +1,44 @@
 
-// var dataRef = new Firebase('https://webdevprojectut.firebaseio.com/');
-//
-// function promptUserName() {
-//   swal({
-//     title: "Hello there!",
-//     text: "Please enter your name:",
-//     type: "input",
-//     showCancelButton: false,
-//     closeOnConfirm: true,
-//     confirmButtonText: "Submit",
-//     inputPlaceholder: "Type your name"},
-//
-//   function(nameInput) {
-//     if (nameInput === false) return false;
-//     if (nameInput === "") {
-//       swal.showInputError("Please enter your name:");
-//       return false;
-//     }
-//     var userData = {
-// 		    name: nameInput
-// 	};
-//     dataRef.push(userData);
-//
-//   });
-// }
-//
-// promptUserName();
+var dataRef = new Firebase('https://webdevprojectut.firebaseio.com/');
+
+var userName;
+
+function promptUserName() {
+  swal({
+    title: "Hello there!",
+    text: "Please enter your name:",
+    type: "input",
+    showCancelButton: false,
+    closeOnConfirm: true,
+    confirmButtonText: "Submit",
+    inputPlaceholder: "Type your name"},
+
+  function(nameInput) {
+    if (nameInput === false) return false;
+    if (nameInput === "") {
+      swal.showInputError("Please enter your name:");
+      return false;
+    }
+    var userData = {
+		    name: nameInput
+	};
+		userName = nameInput;
+		console.log("in function: "+ userName);
+
+    dataRef.push(userData);
+		test(nameInput);
+
+  });
+	// userName = nameInput;
+
+}
+
+function test(name){
+	alert(name);
+}
+
+promptUserName();
+console.log("the name: " + userName);
 
 // Closes Chat Room Box
 	$('.closeChat').on('click',function(){
