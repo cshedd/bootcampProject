fcd2ac6aae7712c812ccf68115d71aac32a5a670
herbertg14 <herbert.gutierrez@utexas.edu>
2016-06-18 02:26:59 -0500

    adding votes by user

--- assets/javascript/usernamePrompt.js
+++ assets/javascript/usernamePrompt.js
@@ -19,14 +19,14 @@ function setUser(name){
     inDatabase = snapshot.child("users/"+ name).exists();
 
     if (inDatabase){
-      alert("user in database");
+      swal("User already exists!");
     }
     else{
       usersRef.child(name).set({
         name: name,
-        vote: 0
+        vote: false
       });
-      alert("user added as: " + name);
+      swal("User added as " + userName + "!");
     }
   });
 }
@@ -88,7 +88,7 @@ function main() {
         swal.showInputError("Please enter your name:");
         return false;
       }
-      // setUser(name);
+      setUser(name);
       userName = name;
       $("#btn-chat").on("click", addMessage);
       chatRef.on("child_added", loadChat);
@@ -121,3 +121,101 @@ $(window).on("resize", function () {
 	}
 	addChatClass();
 }).resize();
+
+
+
+//////////////////////////////////////////////////////////////////
+var placesRef = ref.child("places");
+
+
+function buttons(){
+
+  ref.once("value", function(snapshot){
+    var optionOne = snapshot.child("places/optionOne").val();
+    var optionTwo = snapshot.child("places/optionTwo").val();
+
+    $("#imageOne").append("<img class='img-responsive' value=1 src=" + optionOne.imgFilePath +">");
+    $("#nameOne").html(optionOne.name);
+    $("#voteCount1").html(optionOne.votes);
+
+    $("#imageTwo").append("<img class='img-responsive' value=2 src=" + optionTwo.imgFilePath +">");
+    $("#nameTwo").html(optionTwo.name);
+    $("#voteCount2").html(optionTwo.votes);
+
+
+    $('.imageButton').on('click', function() { 
+
+      swal({title: "Vote Received!",   
+        text: "Thank you for participating",
+        type: "success", confirmButtonColor: "#2ecc71",
+        confirmButtonText: "Close",
+        closeOnConfirm: true });
+
+      //button 1 or 2 
+      var buttonValue = $(this).attr("value");
+
+      //has the user already voted
+      var userSnapshot = snapshot.child("users/" + userName).val();
+      var user = ref.child("users/" + userName);
+
+
+      if (userSnapshot.vote == false){
+        if (buttonValue == 1){
+          var button = ref.child("places/optionOne");
+          var count = snapshot.child("places/optionOne").val().votes;
+          count += 1 
+
+          button.update({
+            votes: count
+          })
+
+          $("#voteCount1").html(button.votes);
+
+          user.update({
+            vote: true
+          })
+
+          console.log("count for one: " + count);
+        }
+      }
+
+      // user.update({
+      //   vote: buttonValue
+      // });
+
+      // if (buttonValue == 1){
+      //   var votes = ref.child("places/optionOne");
+      //   votes.update()  
+      // }
+
+
+    });
+  })
+}
+
+function mainImages(){
+
+  var hour = moment().format("LTS");
+  console.log("the hour is: " + hour);
+
+  //later add that if the date changes so do the two suggestions 
+  //reset the vote 
+  // if the time of day is midnight reset
+
+  // placesRef.set({
+  //  optionOne: {
+  //    name: "Barton Spring Pool",
+  //    imgFilePath: 'assets/images/Barton-Springs.jpg',
+  //    votes: 0
+  //  },
+  //  optionTwo: {
+  //    name: "Hey Cupcake",
+  //    imgFilePath: 'assets/images/Hey-Cupcake.jpg',
+  //    votes: 0 
+  //  }
+  // });
+
+  buttons();
+}
+
+mainImages();
